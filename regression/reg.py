28f191095f33d96a890bd583ef69136ed19f8dcf
tomx3000 <tomx3000@gmail.com>
2018-06-12 16:03:00 +0300

    pickle

--- regression/reg.py
+++ regression/reg.py
@@ -1,8 +1,15 @@
 import pandas as pd 
-import quandl,math
+import quandl,math,datetime
 import numpy as np 
 from sklearn import preprocessing,cross_validation ,svm
 from sklearn.linear_model import LinearRegression
+
+import matplotlib.pyplot as plt 
+from matplotlib import style
+import pickle
+
+
+style.use('ggplot')
 
 class stockRegression:
 	def __init__(self):
@@ -13,9 +20,9 @@ class stockRegression:
 		forecast_col = 'Adj. Close'
 		df.fillna(-99999,inplace=True)
 
-		forecast_out= int(math.ceil(0.01*len(df)))
+		self.forecast_out= int(math.ceil(0.01*len(df)))
 
-		df['label']=df[forecast_col].shift(-forecast_out)
+		df['label']=df[forecast_col].shift(-self.forecast_out)
 
 		df.dropna(inplace=True)
 		# print(df.head)
@@ -31,11 +38,22 @@ class stockRegression:
 		Y=np.array(df['label'])
 
 		X=preprocessing.scale(X)
+		X_sample=X[:-self.forecast_out]
+		Y_sample=Y[:-self.forecast_out]
+
+		self.X_lately=X[-self.forecast_out:]
+
+		X_train,X_test,Y_train,Y_test=cross_validation.train_test_split(X_sample,Y_sample,test_size=0.2)
+
+		# self.clf = LinearRegression(n_jobs=-1)
+
 		
-		X_train,X_test,Y_train,Y_test=cross_validation.train_test_split(X,Y,test_size=0.2)
-		clf = LinearRegression(n_jobs=-1)
-		clf.fit(X_train,Y_train)
-		print(clf.score(X_test,Y_test))
+		# self.clf.fit(X_train,Y_train)
+
+		# classifier().save(filename='linear',clf=self.clf)
+
+		self.clf=classifier().get(filename='linear')
+		print(self.clf.score(X_test,Y_test))
 
 		return self
 		
@@ -45,15 +63,64 @@ class stockRegression:
 
 		pass
 
-	def predict(self):
+	def visualize(self):
+		lastdate= self.df.iloc[-1].name
+		last_unix=lastdate.timestamp()
+		oneday=86400
+		nex_unix=last_unix+oneday
 
+		for i in self.forcastset:
+			next_date=datetime.datetime.fromtimestamp(nex_unix)
+			nex_unix+=oneday
+			self.df.loc[next_date]=[np.nan for _ in range (len(self.df.columns)-1)]+[i]
+
+		self.df['Adj. Close'].plot()
+		self.df['Forecast'].plot()
+		plt.legend(loc=4)
+		plt.xlabel('Date')
+		plt.ylabel('Price')
+		plt.show()	
 
 		pass
+
+	def predict(self):
+		self.forcastset=self.clf.predict(self.X_lately)
+		print(self.forcastset)
+		self.df['Forecast']=np.nan
+
+		return self
 
 	def scoreData(self):
 		pass
 
+class classifier:
+	def __init__(self,*args,**kargs):
+		if(kargs.__contains__('clf')):
+			self.clf=kargs['clf']
+		if(kargs.__contains__('filename')):
+			self.file=kargs['fiename']
+		
+		pass
+
+	def save(self,*args,**kargs):
+		filename=kargs['filename'] if kargs.__contains__('filename') else self.file
+		clf=kargs['clf'] if kargs.__contains__('clf') else self.clf
+
+		with open(filename+'.pickle','wb') as f:
+			pickle.dump(clf,f)
+		return self
+
+	def get(self,*args,**kargs):
+		filename=kargs['filename'] if kargs.__contains__('filename') else self.file
+		clf = open(filename+'.pickle','rb')
+		return pickle.load(clf)
+
+
+
+
+
+
 
 
 test= stockRegression()
-test.getData().prepData()
+test.getData().prepData().predict()
